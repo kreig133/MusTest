b12f8d463e65a060771906678ef7bc2ee776b4e8
Rishat Shamsutdinov <rshamsutdinov@aplana.com>
2012-08-02 17:34:01 +0600

    рефакторинг

---
+++ src/main/java/com/aplana/iask/mus/test/MusTester.java
@@ -0,0 +1,218 @@
+package com.aplana.iask.mus.test;
+
+import com.aplana.iask.mus.test.persistence.dao.MusOpDao;
+import com.aplana.iask.mus.test.persistence.entity.GetOperationDataIn;
+import com.aplana.iask.mus.test.persistence.entity.GetOperationDataOut;
+import com.aplana.iask.mus.test.selenium.IaskSelenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.*;
+
+public class MusTester {
+
+    public static final String OPERATION_PROPS = "operations.properties";
+
+    private static final PrintStream OUT = System.out;
+
+    static {
+//        addOperation(13,	"23ЮИ"); // TODO: unique
+    }
+
+    private final Map<String, List<Integer>> operationsMap;
+    private final MusOpDao musOpDao;
+    private IaskSelenium iaskSelenium;
+    private RemoteWebDriver webDriver;
+
+    public MusTester() {
+        operationsMap = new HashMap<String, List<Integer>>();
+        musOpDao = new MusOpDao();
+    }
+
+    private void addOperation(int opNum, String login) {
+        List<Integer> operationsList = operationsMap.get(login);
+        if (operationsList == null) {
+            operationsList = new ArrayList<Integer>();
+            operationsMap.put(login, operationsList);
+        }
+
+        operationsList.add(opNum);
+    }
+
+    public void test() throws IOException {
+        loadOperations();
+
+        try {
+            iaskSelenium = new IaskSelenium(Settings.get(Settings.IASK_URL));
+            webDriver = iaskSelenium.getDriver();
+
+            webDriver.manage().window().maximize();
+
+            for (String login : operationsMap.keySet()) {
+                if (iaskSelenium.isAuthorized()) {
+                    iaskSelenium.deauthorize();
+                }
+
+                iaskSelenium.waitLoading(1);
+                iaskSelenium.authorization(login);
+                iaskSelenium.waitLoading(2);
+
+                for (Integer operation : operationsMap.get(login)) {
+                    iaskSelenium.closeAnyBox();
+
+                    GetOperationDataIn getOperationDataIn = new GetOperationDataIn();
+                    getOperationDataIn.setOpNum(operation);
+                    getOperationDataIn.setLogin(login);
+
+                    GetOperationDataOut getOperationDataOut = musOpDao.getOperationData(getOperationDataIn);
+
+                    try {
+                        openAgreement(getOperationDataOut);
+
+                        OUT.println(String.format("Операция %d - OK", operation));
+                    } catch (Exception e) {
+                        OUT.println(String.format(
+                                "Ошибка выполнения операции %d (%s): %s\n* Пользователь: %s\n* Договор: %s",
+                                operation, getOperationDataOut.getOpName(), e.getMessage(), login,
+                                getOperationDataOut.getPackTitle()));
+                    }
+                }
+            }
+        } catch (Throwable th) {
+            OUT.println(th.toString());
+        } finally {
+            if (iaskSelenium != null) {
+                iaskSelenium.close();
+            }
+        }
+    }
+
+    private void loadOperations() throws IOException {
+        Properties properties = new Properties();
+
+        FileInputStream fileInputStream = new FileInputStream(OPERATION_PROPS);
+        properties.load(fileInputStream);
+        fileInputStream.close();
+
+        for (String propertyName : properties.stringPropertyNames()) {
+            addOperation(Integer.valueOf(propertyName), properties.getProperty(propertyName));
+        }
+    }
+
+    private void openAgreement(GetOperationDataOut getOperationDataOut) throws Exception {
+        WebElement element;
+
+        findAgreement(getOperationDataOut);
+
+        Thread.sleep(50);
+
+        editSelectedAgreementInMus();
+
+        boolean tabNotSwitched = true;
+        try {
+            while (true) {
+                List<WebElement> elements = webDriver.findElementsByXPath
+                        (
+                                "//div[@class = 'x-panel-bwrap']//table[@class = 'x-grid3-row-table']" +
+                                        "//tr[@role = 'presentation']//button/ancestor::tr//div[@class = 'x-grid3-cell-inner x-grid3-col-name']"
+                        );
+
+                final int size = elements.size();
+
+                if (size == 0) {
+                    checkForErrorBox();
+                    throw new Exception("Нет операций, доступных для выполнения.");
+                }
+
+                for (int i = 0; i < size; i++) {
+                    element = elements.get(i);
+                    if (element.getText().equals(getOperationDataOut.getOpName())) {
+                        new Actions(webDriver).doubleClick(element).build().perform();
+
+                        iaskSelenium.waitLoading(3);
+
+                        checkForErrorBox();
+
+                        return;
+                    }
+                }
+                if (tabNotSwitched) {
+                    tabNotSwitched = false;
+                    element = webDriver.findElement(
+                            By.xpath("//li[not(contains(@class, 'x-tab-strip-active'))]//span[starts-with(text(), " +
+                                    "'<<')]/ancestor::li"));
+                    element.click();
+                    iaskSelenium.waitLoading(2);
+                } else {
+                    break;
+                }
+            }
+        } finally {
+            webDriver.switchTo().defaultContent();
+        }
+        throw new Exception("Операция недоступна для выполнения.");
+    }
+
+    private void checkForErrorBox() throws Exception {
+        WebElement element = null;
+        try {
+            element = webDriver.findElement(By.xpath("//span[text() = 'Предупреждение " +
+                    "системы']/ancestor::div[normalize-space(@class) = 'x-window x-component']/div[@class = 'x-window-bwrap']//div[@class = 'gwt-Label x-component']"));
+
+        } catch (org.openqa.selenium.NoSuchElementException nsee) {
+            try {
+                element = webDriver.findElement(By.xpath("//span[text() = 'Ошибка']/ancestor::div[" +
+                        "normalize-space(@class) = 'x-window x-component']/div[@class = 'x-window-bwrap']//div[@class = 'gwt-HTML' and position() > 1]"));
+            } catch (NoSuchElementException e) {
+            }
+        }
+
+        if (element != null) {
+            throw new Exception(element.getText());
+        }
+    }
+
+    private void editSelectedAgreementInMus() throws InterruptedException {
+        WebElement element = webDriver.findElementByXPath("//button[text() = 'Редактировать в МУС']");
+        iaskSelenium.alternativeClick(element);
+
+        iaskSelenium.waitLoading(4);
+
+        webDriver.switchTo().frame(webDriver.findElementByXPath("//iframe[@class = 'gwt-Frame x-component']"));
+
+        iaskSelenium.waitLoading(1);
+
+        iaskSelenium.closeAnyBox();
+    }
+
+    private void findAgreement(GetOperationDataOut getOperationDataOut)
+            throws Exception {
+        iaskSelenium.workWithMenu(new String[] {"Договор", "Открыть договор"});
+        iaskSelenium.waitLoading(2);
+
+        WebElement element = webDriver.findElementByXPath("//div[text() = 'Номер договора']/ancestor::tr//input[1]");
+        element.clear();
+        element.sendKeys(getOperationDataOut.getPackTitle());
+
+        element = webDriver.findElementByXPath("//button[text() = 'Применить']");
+        iaskSelenium.alternativeClick(element);
+        iaskSelenium.waitLoading(4);
+
+        iaskSelenium.closeAnyBox();
+
+        try {
+            element = webDriver.findElementByXPath(
+                    "//div[@class = 'x-grid3-cell-inner x-grid3-col-sPackTitle' and text() = '" +
+                            getOperationDataOut.getPackTitle() + "']");
+            element.click();
+        } catch (NoSuchElementException e) {
+            throw new Exception(String.format("Договор %s не найден.", getOperationDataOut.getPackTitle()));
+        }
+    }
+}
